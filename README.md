0d3b882439e4ec5597ae97da815e872722a11ceb
Matt Campbell <mecampbell25@gmail.com>
2014-06-30 00:39:52 -0400

    Fixing integration spec

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Using Ruby + D3.js, provide a real-time visualization of public GitHub repo commits ***by-language***.
 
 ## Next Steps
+
 * implement basic GitHub API client (using octokit gem, perhaps?)
 * determine process for fetching, in real-time, most recent GitHub commits
 * determine how to extract language(s) data from commit object
--- spec/integration/fetch_user_repos_spec.rb
+++ spec/integration/fetch_user_repos_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "#repositories" do
+describe "#repos_for" do
   let(:token)  { ENV['GH_ACCESS_TOKEN'] }
   let(:client) { GithubLangs::Client.new(token) }
 
   it "returns a list of repositories for a given github user" do
-    user = client.user "mecampbellsoup"
-    expect(user.repositories).to include(["btc_price"]) 
+    repos = client.repos_for "mecampbellsoup"
+    repos.must_include("https://github.com/mecampbellsoup/btc_price") 
   end
 end
